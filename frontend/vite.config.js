8b03e1862701f141c62363b2011bc1e24a0d3558
sampathkuamr132 <sampathkumar.s@acheron-tech.com>
2026-01-04 22:26:14 +0530

    deployment docker files added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: [
-      'ec2-13-60-228-255.eu-north-1.compute.amazonaws.com'
-    ]
-  }
 })
